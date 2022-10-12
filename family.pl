cb445779e55110155f9e14df9bb5db947e10b60d
bartw <bartwesthoff2002@gmail.com>
2022-10-12 21:19:01 +0200

    tweede commit

--- family.pl
+++ family.pl
@@ -15,3 +15,46 @@ parent(fien,merlijn).
 parent(peter,fien).
 parent(peter,joost).
 isChild(X) :- parent(Y,X).
+
+
+sibling(X, Y) :-
+      parent(Z, X),
+      parent(Z, Y),
+      X \= Y.
+
+
+% stap 3
+brother(X, Y) :-
+      sibling(X, Y),
+      X\=Y,
+      male(X).
+
+% stap 3 extra
+sister(X, Y) :-
+      sibling(X, Y),
+      X\=Y,
+      female(X).
+
+
+
+father(X,Y) :-
+    male(X),
+    parent(X,Y).
+
+mother(X,Y) :-
+    female(X),
+    parent(X,Y).
+
+% stap 5
+cousin(X,Y) :-
+    parent(Z,X),
+    parent(W,Y),
+    sibling(Z,W).
+
+% stap 6
+ancestor(X,Y) :-
+    parent(X,Y).
+
+family(X,Y) :-
+    ancestor(X,Y) ; sibling(X,Y) ; cousin(X,Y),
+    X\=Y.
---
+++ wumpus.pl
@@ -0,0 +1,31 @@
+:-retractall(robot(_)).
+:-dynamic robot/1.
+
+%The location of robot and goal
+robot(1).
+goal(5).
+%The first link of the predicate
+link(1,2).
+link(2,3).
+
+link(3,4).
+link(3,6).
+
+link(6,7).
+link(6,5).
+
+adjacent(L) :- robot(R), link(R,L).
+suggest(L) :- adjacent(L), goal(L).
+%suggest(L) :- adjacent(L), \+robot(L), goal(L).
+move(L) :- adjacent(L), retract(robot(_)), assert(robot(L)).
+
+tp(L) :- retract(robot(_)), assert(robot(L)).
+
+
+% 14. check if there is a path from the current location to the goal (if robot at 4 or 7 it does not exists)
+%path() :- robot(R), goal(G), \+ (R = G) ; link(R,NEW)-> move(NEW), path().
+path :- robot(R), goal(G), \+ (R = G) ; link(R,NEW) ; link(NEW,_)-> move(NEW), path().
+
+path(P) :- robot(R), goal(G), \+ (R = G) ; link(R,NEW) ; link(NEW,_)-> move(NEW), path(P).
+
+append ( [ X | Y ] , Z, [X | W] ) :- append (Y , Z, W) .
